773bfc675fc3e57b6eb1276046cfc35c4aea480e
Filipe Maia <filipebsmaia@gmail.com>
2025-04-04 16:22:02 -0300

    feat: convert unit-of-work-prisma to extends unit-of-work as class

--- src/@core/common/infra/unit-of-work-prisma.ts
+++ src/@core/common/infra/unit-of-work-prisma.ts
@@ -1,8 +1,12 @@
 import { PrismaManager } from "@/database/prisma-manager";
 import { UnitOfWork } from "@core/common/application/unit-of-work";
+import { AggregateRoot } from "../domain/aggregate-root";
 
-export class PrismaUnitOfWork implements UnitOfWork {
-  constructor(private readonly prisma: PrismaManager) {}
+export class PrismaUnitOfWork extends UnitOfWork {
+  
+  constructor(private readonly prisma: PrismaManager) {
+    super();
+  }
 
   async runTransaction<T>(fn: () => Promise<T>): Promise<T> {
     return this.prisma.runTransaction(async () => {
